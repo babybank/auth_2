0daf8b55c917cc87a4ad7094600d772cd7bada5a
unknown <babybank40@gmail.com>
2019-03-05 12:20:41 +0700

    first commit

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 // npm install crypto-js --save
 // npm install jsonwebtoken@8.0.1 --save
 // npm install cookie-parser
+
+// git init
+// git add .
+// git status
+// git commit -m "first commit"
+// git remote add origin git@github.com:babybank/auth_2.git
+// git push -u origin master
